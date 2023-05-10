8dd312261c1bd020713ed046397efe95a98dddc3
AniMB <120055908+AniMB@users.noreply.github.com>
2023-05-11 00:51:57 +0530

    Update data_storage.py

--- data_storage.py
+++ data_storage.py
@@ -25,7 +25,7 @@ for i in range(5):
 entered_data=gui.data_entry()
 value={}
 for i in entered_data:
-        if entered_data[i]!='rpm5_':
+        if entered_data[i]!='vibz_': #change this to the correct variable
                 value[i]=entered_data[i]
         else:
                 break
@@ -40,9 +40,9 @@ rated=[]
 def total_head(x,y,z):
         return lambda x,y,z:x+y+z
 for i in inputted_si:
-        rated.append(formulas.ratedp(i[0],value['rpm_'],i[1]))
+        rated.append(formulas.ratedq(i[0],value['rpm_'],i[3]))
         rated.append(formulas.ratedh(i[0],value['rpm_'],total_head(i[1],i[2],value['guageHeight_'])))
         rated.append(formulas.ratedp(i[0],value['rpm_'],(value['mototEffi_']*i[5])))
         rated.append(formulas.efficiency(rated[0],rated[1],value['Density_'],value['motorEffi_'],i[5])) 
                
-graph.grapher(inputted_si,value['guageHeight_'])  
+graph.grapher(rated,value['guageHeight_'])  
