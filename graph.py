794b32c888c0211b404ca6ee41775aac78be55b3
AniMB <120055908+AniMB@users.noreply.github.com>
2023-05-15 23:44:31 +0530

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,8 +1,9 @@
-def grapher(data):
+def grapher(data, capacity, head):
     import matplotlib.pyplot as plt
     from matplotlib import style
     import numpy as np
     from scipy.interpolate import make_interp_spline
+    
     
     
     x1=[]
@@ -45,12 +46,13 @@ def grapher(data):
     Y3_ = X_Y3_Spline(X_)
     
     
-    p1,=ax.plot(X_, Y1_, 'g-',label='Head')
-    ax.scatter(x1, y1, c='g', alpha=0.5, marker='o') # type: ignore
-    p2,=ax1.plot(X_,Y2_,'b-', label='Power')
-    ax1.scatter(x1, y2, c='b', alpha=0.5, marker='p') # type: ignore
-    p3,=ax2.plot(X_,Y3_,'r-', label='Efficiency')
-    ax2.scatter(x1, y3, c='r', alpha=0.5, marker='x') # type: ignore
+    ax.plot(X_, Y1_, 'g-')
+    p1=ax.scatter(x1, y1, c='g', alpha=0.5, marker='o',label='Head') # type: ignore
+    ax1.plot(X_,Y2_,'b-')
+    p2=ax1.scatter(x1, y2, c='b', alpha=0.5, marker='p', label='Power') # type: ignore
+    ax2.plot(X_,Y3_,'r-')
+    p3=ax2.scatter(x1, y3, c='r', alpha=0.5, marker='x', label='Efficiency') # type: ignore
+    p4=ax.scatter(capacity,head, marker=11, label='Guarenteed Value') # type: ignore
     
     ax.set_xlim(0, (max(x1)*1.1))
     ax.set_ylim(0, (max(y1)*1.1))
@@ -58,39 +60,17 @@ def grapher(data):
     ax2.set_ylim(0, (max(y3)*1.1))
     
     
+   
+    ax.set_xlabel('Capacity (m\u00b3/hr)')
+    ax.set_ylabel('Head(m)')
+    ax1.set_ylabel('Power(kW)')
+    ax2.set_ylabel('Efficiency(%)')
     
-    ax.set_xlabel('Capacity')
-    ax.set_ylabel('Head')
-    ax1.set_ylabel('Power')
-    ax2.set_ylabel('Efficiency')
     
-    
-    ax.legend(handles=[p1, p2, p3])
-    def map_range(value, old_min, old_max, new_min, new_max):
-    
-    # Maps a value from the old range [old_min, old_max] to the new range [new_min, new_max]
-    
-        old_range = old_max - old_min
-        new_range = new_max - new_min
-        scaled_value = (value - old_min) / old_range
-        return new_min + (scaled_value * new_range)
+    handles = [p1, p2,p3,p4]
+    labels = [p1.get_label(), p2.get_label(), p3.get_label(), p4.get_label()]
+    plt.legend(handles, labels,  loc='lower right')
 
-
-    def onclick(event,x1,y3):
-                   
-        duty_point_x= event.xdata
-        duty_point_y=event.ydata
-        ax.plot([duty_point_x,duty_point_x],[duty_point_y, 0], 'C8--')
-        plt.plot([duty_point_x, 0],[duty_point_y, duty_point_y], 'C8--')
-        ax.annotate(f'{map_range(duty_point_x,0, (max(y3)),0, (max(x1))):.2f}', xy=(duty_point_x, 0), xytext=(duty_point_x+0.5, 0.5), )
-        plt.annotate(f'{duty_point_y:.2f}', xy=(0, duty_point_y), xytext=(0.5, duty_point_y+0.5))
-        plt.scatter([duty_point_x], [duty_point_y], s=100)
-        plt.show()
-    
-    ax.figure.canvas.mpl_connect('button_press_event', lambda event :onclick(event, x1, y3))
-        
-        
-    
     
     plt.grid(True, color='#5F2D9A')
     plt.show()
@@ -98,3 +78,30 @@ def grapher(data):
 # refer to this'https://matplotlib.org/3.4.3/gallery/ticks_and_spines/multiple_yaxis_with_spines.html'
 # and https://stackoverflow.com/questions/58337823/matplotlib-how-to-display-and-position-2-yaxis-both-on-the-left-side-of-my-grap
 # bottom limit should be zero for all
+
+#Useless Code i didn't wanna waste cause I put in too much hardwork
+    # def map_range(value, old_min, old_max, new_min, new_max):
+    
+    # # Maps a value from the old range [old_min, old_max] to the new range [new_min, new_max]
+    
+    #     old_range = old_max - old_min
+    #     new_range = new_max - new_min
+    #     scaled_value = (value - old_min) / old_range
+    #     return new_min + (scaled_value * new_range)
+
+
+    # def onclick(event,x1,y3):
+                   
+    #     duty_point_x= event.xdata
+    #     duty_point_y=event.ydata
+    #     ax.plot([duty_point_x,duty_point_x],[duty_point_y, 0], 'C8--')
+    #     plt.plot([duty_point_x, 0],[duty_point_y, duty_point_y], 'C8--')
+    #     ax.annotate(f'{map_range(duty_point_x,0, (max(y3)),0, (max(x1))):.2f}', xy=(duty_point_x, 0), xytext=(duty_point_x+0.5, 0.5), )
+    #     plt.annotate(f'{duty_point_y:.2f}', xy=(0, duty_point_y), xytext=(0.5, duty_point_y+0.5))
+    #     plt.scatter([duty_point_x], [duty_point_y], s=100)
+    #     plt.show()
+    
+    # ax.figure.canvas.mpl_connect('button_press_event', lambda event :onclick(event, x1, y3))
+        
+        
+    
