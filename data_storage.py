d4fe6e8aae029d5f77b81082784876e9c47ccacd
AniMB <120055908+AniMB@users.noreply.github.com>
2023-05-16 12:10:10 +0530

    Update data_storage.py

--- data_storage.py
+++ data_storage.py
@@ -1,3 +1,7 @@
+# Author: Animish Murthy
+# Date: 18/05/23
+
+
 import serial
 import csv
 import graph
@@ -11,6 +15,8 @@ ser = serial.Serial(arduino_port, baud)
 print("Connected to Arduino port:" + arduino_port)
 
 
+# Author: Animish Murthy
+# Date: 18/05/23
 
 inputted=[]
 for i in range(5):
@@ -20,13 +26,8 @@ for i in range(5):
         inputted.append(datastring)
         
 
-entered_data=gui.data_entry()
-value={}
-for i in entered_data:
-        if entered_data[i]!='vibz_': #change this to the correct variable
-                value[i]=entered_data[i]
-        else:
-                break
+value=gui.data_entry()
+
 
 inputted_si=inputted
 for i in inputted:
@@ -34,6 +35,8 @@ for i in inputted:
         delivery=formulas.bar_to_m_h2o(i[2])
         suction=inputted_si[1]
         delivery=inputted_si[2]
+        
+
 rated=[]
 def total_head(x,y,z):
         return lambda x,y,z:x+y+z
@@ -42,7 +45,11 @@ for i in inputted_si:
         rated.append(formulas.ratedh(i[0],value['rpm_'],total_head(i[1],i[2],value['guageHeight_'])))
         rated.append(formulas.ratedp(i[0],value['rpm_'],(value['mototEffi_']*i[5])))
         rated.append(formulas.efficiency(rated[0],rated[1],value['Density_'],value['motorEffi_'],i[5])) 
-               
+
+# Author: Animish Murthy
+# Date: 18/05/23
+
+              
 final=inputted_si
 for k in final:
         final[k].insert(1,inputted[k][1])
